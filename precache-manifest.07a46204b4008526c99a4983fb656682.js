735cc8ec614b5cad0d1d35484a11a256b54e1fe8
FoxPro111 <yurec007@list.ru>
2019-06-05 07:57:59 +0300

    Updates

---
+++ precache-manifest.07a46204b4008526c99a4983fb656682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6b22fd94a4d4839ac6c98566c4c4c7",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "29c5cef3858515643dfa",
+    "url": "/aviasales/static/css/main.36b614b8.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/aviasales/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "29c5cef3858515643dfa",
+    "url": "/aviasales/static/js/main.d8693dc7.chunk.js"
+  },
+  {
+    "revision": "af052bb48cec26b9b243",
+    "url": "/aviasales/static/js/runtime~main.f8f776b2.js"
+  },
+  {
+    "revision": "52cf67e82d7248784ca46d657f0f2562",
+    "url": "/aviasales/static/media/svg.52cf67e8.svg"
+  }
+]);
